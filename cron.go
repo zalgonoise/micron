ba79737ba4440eb453ea95d564afe840cf103b6c
zalgonoise <mariozalgo@gmail.com>
2025-04-02 00:31:59 +0200

    micron: refactor interface declarations

--- cron.go
+++ cron.go
@@ -7,7 +7,10 @@ import (
 	"github.com/zalgonoise/cfg"
 	"github.com/zalgonoise/x/errs"
 	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
 
+	"github.com/zalgonoise/micron/log"
+	"github.com/zalgonoise/micron/metrics"
 	"github.com/zalgonoise/micron/selector"
 )
 
@@ -48,6 +51,31 @@ type Runtime interface {
 	Err() <-chan error
 }
 
+// Selector describes the capabilities of a cron selector, which picks up the next job to execute (out of a set of
+// executor.Executor)
+//
+// Implementations of Selector must focus on the logic within its only method, Next, that will set the strategy to
+// picking up the following job to run. The default implementation looks for the nearest job (in time) to execute, with
+// support for multiple executions in one-go.
+//
+// Custom implementations could, for example, check for preconditions, run clean-up jobs, and more.
+//
+// The runtime of a Selector depends on the input context.Context when calling its Next method, as it can be used to
+// signal cancellation or used for timeouts.
+type Selector interface {
+	// Next picks up the following scheduled job to execute from its configured (set of) executor.Executor, and
+	// calls its Exec method.
+	//
+	// This call also imposes a minimum step duration of 50ms, to ensure that early-runs are not executed twice due to the
+	// nature of using clocks in Go. This sleep is deferred to come in after the actual execution of the job.
+	//
+	// The Selector allows multiple executor.Executor to be configured, and multiple executor.Executor can share similar
+	// execution times. If that is the case, the executor is launched in an executor.Multi call.
+	//
+	// The error returned from a Next call is the error raised by the executor.Executor's Exec call.
+	Next(ctx context.Context) error
+}
+
 // Metrics describes the actions that register Runtime-related metrics.
 type Metrics interface {
 	// IsUp signals whether the Runtime is running or not.
@@ -55,7 +83,7 @@ type Metrics interface {
 }
 
 type runtime struct {
-	sel selector.Selector
+	sel Selector
 
 	err chan error
 
@@ -134,14 +162,32 @@ func newRuntime(config *Config) (Runtime, error) {
 		size = defaultBufferSize
 	}
 
-	return runtime{
+	r := runtime{
 		sel: config.sel,
 		err: make(chan error, size),
 
 		logger:  slog.New(config.handler),
 		metrics: config.metrics,
 		tracer:  config.tracer,
-	}, nil
+	}
+
+	if config.handler == nil {
+		config.handler = log.NoOp()
+	}
+
+	r.logger = slog.New(config.handler)
+
+	if config.metrics == nil {
+		config.metrics = metrics.NoOp()
+	}
+
+	r.metrics = config.metrics
+
+	if config.tracer == nil {
+		config.tracer = noop.NewTracerProvider().Tracer("no-op tracer")
+	}
+
+	return r, nil
 }
 
 // NoOp returns a no-op Runtime.
